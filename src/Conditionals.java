74101a0fc4cab1dd01fce1ef0f776a49cfc54cf3
Wayne Kellman <waynekellman@ac.c4q.nyc>
2018-01-06 14:40:02 -0500

    conditionals

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -21,7 +21,109 @@ public class Conditionals {
     longName(p);
     b = cigarParty(61,true);
     System.out.println(b);
+    int n = caughtSpeeding(81, false);
+    System.out.println(n);
+    String s = alarmClock(1, false);
+    System.out.println(s);
+    n = greenLottery(10,120,0);
+    System.out.println(n);
+    n = blackJack(22,19);
+    System.out.println(n);
+    b= evenlySpaced(2,6,4);
+    System.out.println(b);
+  }
 
+  private static boolean evenlySpaced(int i, int i1, int i2) {
+    int dif1 = i - i1;
+    int dif2 = i1 - i2;
+
+    if (dif1 == dif2){
+      return true;
+    }
+    return false;
+  }
+
+  private static int blackJack(int i, int i1) {
+    //idk what to do if they are the same number so i'm just returning 0.
+    int dif1 = 21 - i;
+    int dif2 = 21 - i1;
+    if (i > 21 && i1 > 21){
+      return 0;
+    } else if (i > 21){
+      return i1;
+    } else if (i1 > 21){
+      return i;
+    }
+    if (dif1 > dif2){
+      return i1;
+    } else if (dif2 > dif1){
+      return i;
+    }
+    return 0;
+  }
+
+  private static int greenLottery(int i, int i1, int i2) {
+    if(i == i2&& i == i1){
+      return 20;
+    }else if (i1 == i || i1 == i2 || i == i2){
+      return 10;
+    } else {
+      return 0;
+    }
+  }
+
+  private static String alarmClock(int i, boolean b) {
+    switch (i){
+      case 0:
+          return "off";
+      case 1:
+        if (!b){
+          return "7:00";
+        } else {
+          return "10:00";
+        }
+      case 2:
+        if (!b){
+          return "7:00";
+        } else {
+          return "10:00";
+        }
+      case 3:
+        if (!b){
+          return "7:00";
+        } else {
+          return "10:00";
+        }
+      case 4:
+        if (!b){
+          return "7:00";
+        } else {
+          return "10:00";
+        }
+      case 5:
+        if (!b){
+          return "7:00";
+        } else {
+          return "10:00";
+        }
+      case 6:
+          return "off";
+      default:
+          return "not valid";
+    }
+  }
+
+  private static int caughtSpeeding(int i, boolean b) {
+    if (b){
+      return 0;
+    }
+    if(i <= 60) {
+      return 0;
+    } else if (i <= 80){
+      return 1;
+    } else {
+      return 2;
+    }
   }
 
   private static boolean cigarParty(int i, boolean b) {
